962c3ee3ebfa1638d3f535fc1d73e0dc986a3eac
Donato-Carola <don94@live.it>
2023-12-13 14:39:09 +0100

    prima prova fatta

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+
+
+
+<script>
+ export default{
+    
+ }
+  
+
+</script>
+
+<template>
+
+<div>
+    <h1>
+        La mia prima App con vite!
+    </h1>
+</div>
+
+</template>
+
+<style scoped>
+</style>
